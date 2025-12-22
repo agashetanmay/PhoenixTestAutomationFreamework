536001bd20ad00df8cbc0cb0cbdd6b15766a9ffc
tanmay <agashetanmay@gmail.com>
2025-12-22 12:12:08 +0530

    created POJO for create job api and added utility method to pass auth token and payload

--- src/test/java/com/api/Utils/SpecUtil.java
+++ src/test/java/com/api/Utils/SpecUtil.java
@@ -58,6 +58,23 @@ public class SpecUtil {
 				return requestSpecification;
 
 	}
+	
+      public static RequestSpecification requestSpecificationWithAuthAndPayload(Role role, Object Payload) {
+		
+		RequestSpecification requestSpecification = new RequestSpecBuilder()
+				.setBaseUri(configManager.getProperty("BASE_URI"))
+				.setContentType(ContentType.JSON)
+				.setAccept(ContentType.JSON)
+				.addHeader("Authorization", authTokenProvider.getToken(role.FD))
+				.setBody(Payload)
+				.log(LogDetail.URI)
+				.log(LogDetail.METHOD)
+				.log(LogDetail.HEADERS)
+				.log(LogDetail.BODY)
+				.build();
+				return requestSpecification;
+
+	}
 	public static ResponseSpecification responseSpec_OK() {
 		
 		ResponseSpecification responsespecification = new ResponseSpecBuilder()
